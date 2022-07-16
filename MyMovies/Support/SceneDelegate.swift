80a893039dfc8cc77da21bdba5b4f9c4085be652
AdnanJoraid <AdnanJoraidca@gmail.com>
2022-07-16 17:20:12 -0400

    Initial commit. Added folders, tabbar and essential main view controllers

--- MyMovies/Support/SceneDelegate.swift
+++ MyMovies/Support/SceneDelegate.swift
@@ -16,9 +16,36 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = createTabBar()
+        window?.makeKeyAndVisible()
+        
     }
-
+    
+    private func createMoviesNC() -> UINavigationController {
+        let moviesVC = MoviesVC()
+        moviesVC.title = "Movies"
+        moviesVC.tabBarItem.image = UIImage(systemName: "tv")
+        return UINavigationController(rootViewController: moviesVC)
+    }
+    
+    private func createFavoriteMoviesNC() -> UINavigationController {
+        let favoriteMoviesVC = FavoriteMoviesVC()
+        favoriteMoviesVC.title = "Favorites"
+        favoriteMoviesVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        return UINavigationController(rootViewController: favoriteMoviesVC)
+    }
+    
+    private func createTabBar() -> UITabBarController {
+        let tabbar = UITabBarController()
+        UITabBar.appearance().tintColor = .systemRed
+        tabbar.viewControllers = [createMoviesNC(), createFavoriteMoviesNC()]
+        return tabbar
+    }
+    
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
---
+++ MyMovies/ViewConrtoller/FavoriteMoviesVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteMoviesVC.swift
+//  MyMovies
+//
+//  Created by Adnan Joraid on 2022-07-16.
+//
+
+import UIKit
+
+class FavoriteMoviesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
