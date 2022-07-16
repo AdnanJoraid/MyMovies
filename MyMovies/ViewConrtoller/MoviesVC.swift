e5c9d360fb92808cf311e5c367623e859a49c8ce
AdnanJoraid <AdnanJoraidca@gmail.com>
2022-07-16 17:25:14 -0400

    Added view controllers base color

--- MyMovies/ViewConrtoller/MoviesVC.swift
+++ MyMovies/ViewConrtoller/MoviesVC.swift
@@ -8,9 +8,12 @@
 import UIKit
 
 class MoviesVC: UIViewController {
-
+    var label = UILabel(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .black
+        
     }
 
 }
